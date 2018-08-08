26c283ac644a1f96c9c532820ae3a2621910e3f4
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-08-08 12:36:46 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,14 +4,10 @@ def my_collect(array)
   while i < array.length 
      yield array[i]
     i += 1 
-    array.push("#{my_collect(array)}")
-    array.shift
   end
-  array
 end
 
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 my_collect(array) do |name|
-  array.push"#{name.split(" ").first}"
-  array.shift
+  name.split(" ").first
 end
